63c675e9531da5ea06c49869358a6d6950dba8dd
Christian Mendoza <christianmendozater@gmail.com>
2024-07-17 00:42:56 -0400

    fix tailwind errors and header.vue

--- mini-chat/src/components/Header.vue
+++ mini-chat/src/components/Header.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-
-    </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+    <header class="bg-muted px-4 py-2 flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="w-6 h-6"
+        >
+          <circle cx="12" cy="10" r="8"></circle>
+          <circle cx="12" cy="10" r="3"></circle>
+          <path d="M7 22h10"></path>
+          <path d="M12 22v-4"></path>
+        </svg>
+        <span class="font-medium">Mini Chat</span>
+      </div>
+    </header>
+  </template>
+  
+  <script setup>
+  </script>
+  
