e4796b88bd2763022688d46d26e1f5c54fad43fc
Christian Mendoza <christianmendozater@gmail.com>
2024-07-17 00:38:12 -0400

    create components and app.vue with the main components

--- mini-chat/src/App.vue
+++ mini-chat/src/App.vue
@@ -1,30 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col h-[90vh] max-w-2xl mx-auto bg-background rounded-2xl shadow-lg overflow-hidden">
+    <Header />
+    <MessageList />
+    <InputForm />
+    <Footer />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import Header from './components/Header.vue'
+import MessageList from './components/MessageList.vue'
+import InputForm from './components/InputForm.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style scoped>\
 </style>
